735c67244040c49d32f2b83947dac36247bf88d5
cloverdefa <cloverdefa@gmail.com>
2021-10-18 21:43:29 +0800

    修正usermod帳號

--- docker-install.sh
+++ docker-install.sh
@@ -1,3 +1,3 @@
 sudo sh ~/docker/get-docker.sh    #執行下載的docker安裝捷徑
-sudo usermod -aG docker (whoami)    #給予docker使用者帳號權限
+sudo usermod -aG docker $(whoami)    #給予docker使用者帳號權限
 sudo reboot     #重開機使設定生效
