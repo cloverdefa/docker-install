00c02e612c7abc69dbd924d0a331b27f1baa2713
cloverdefa <cloverdefa@gmail.com>
2021-10-18 21:37:25 +0800

    加上註解

--- docker-install.sh
+++ docker-install.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://get.docker.com -o get-docker.sh    #下載docker官方安裝捷徑
 sudo sh get-docker.sh    #執行下載的docker官方安裝捷徑
-sudo usermod -aG docker (whoami)
-sudo reboot
+sudo usermod -aG docker (whoami)    #給予docker使用者帳號權限
+sudo reboot     #重開機使設定生效
