2fa108a967ac343440f51a14d0138a83f7474aae
cloverdefa <cloverdefa@gmail.com>
2021-10-19 19:03:03 +0800

    修正sleep順序

--- docker-install.sh
+++ docker-install.sh
@@ -11,16 +11,17 @@ sudo usermod -aG docker $(whoami)  #添加Docker 權限賦予使用者
 sleep 2s
 docker version  #檢查Docker安裝版本
 echo "OK!Docker安裝完畢 \n"
+sleep 3s
 echo "開始安裝Docker-compose \n"
 sleep 3s
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  #下載docker-compose官方檔案
-sleep 2s
+sleep 3s
 sudo chmod +x /usr/local/bin/docker-compose  #賦予docker-compose執行權限
-sleep 2s
+sleep 3s
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose  #建立docker-compose運行捷徑
-sleep 2s
-sleep 2s
+sleep 3s
 docker-compose --version   #檢查docker-compose安裝版本
+sleep 3s
 echo "Docker-compose安裝完成，3秒後重新啟動系統以使設定及安裝生效"
 sleep 3s
 sudo rm -r ~/docker   #清理下載資料夾
