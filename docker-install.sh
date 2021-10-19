a80232024bffa21dcbdd38b64952337bbc60f832
cloverdefa <cloverdefa@gmail.com>
2021-10-19 18:13:10 +0800

    更新docker檔案執行路徑

--- docker-install.sh
+++ docker-install.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://get.docker.com -o get-docker.sh    #下載官方docker安裝捷徑
-sudo sh get-docker.sh    #執行下載的docker安裝捷徑
+sudo sh /home/$(whoami)/docker/get-docker.sh    #執行下載的docker安裝捷徑
 sudo usermod -aG docker $(whoami)    #給予docker使用者帳號權限
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose    #下載docker-compose
 sudo chmod +x /usr/local/bin/docker-compose   #賦予docker-compose執行權限
